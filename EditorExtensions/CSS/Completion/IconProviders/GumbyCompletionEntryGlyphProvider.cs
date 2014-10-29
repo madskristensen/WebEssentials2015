81fb7d5dbafabb00dee1bea7c41bf530c1c981d3
Mads Kristensen <post@madskristensen.net>
2014-10-29 16:38:14 -0700

    CSS: Added logo icons to completion

---
+++ EditorExtensions/CSS/Completion/IconProviders/GumbyCompletionEntryGlyphProvider.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CSS.Editor;
+using Microsoft.Web.Editor.Intellisense;
+using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MadsKristensen.EditorExtensions.CSS
+{
+    [Export(typeof(ICssCompletionEntryGlyphProvider))]
+    class GumbyCompletionEntryGlyphProvider : ICssCompletionEntryGlyphProvider
+    {
+        private static BitmapFrame _icon = BitmapFrame.Create(new Uri("pack://application:,,,/WebEssentials2015;component/Resources/Images/gumby.png", UriKind.RelativeOrAbsolute));
+        private static Regex _regex = new Regex(@"^gumby(-.*)?(\.min)?\.css$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        public ImageSource GetCompletionGlyph(string entryName, Uri sourceUri, CssNameType nameType)
+        {
+            if (sourceUri == null)
+            {
+                return null;
+            }
+
+            string filename = Path.GetFileName(sourceUri.ToString()).Trim();
+
+            if (_regex.IsMatch(filename))
+            {
+                return _icon;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ EditorExtensions/CSS/Completion/IconProviders/SkeletonCompletionEntryGlyphProvider.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CSS.Editor;
+using Microsoft.Web.Editor.Intellisense;
+using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MadsKristensen.EditorExtensions.CSS
+{
+    [Export(typeof(ICssCompletionEntryGlyphProvider))]
+    class SkeletonCompletionEntryGlyphProvider : ICssCompletionEntryGlyphProvider
+    {
+        private static BitmapFrame _icon = BitmapFrame.Create(new Uri("pack://application:,,,/WebEssentials2015;component/Resources/Images/skeleton.png", UriKind.RelativeOrAbsolute));
+        private static Regex _regex = new Regex(@"^skeleton(-.*)?(\.min)?\.css$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        public ImageSource GetCompletionGlyph(string entryName, Uri sourceUri, CssNameType nameType)
+        {
+            if (sourceUri == null)
+            {
+                return null;
+            }
+
+            string filename = Path.GetFileName(sourceUri.ToString()).Trim();
+
+            if (_regex.IsMatch(filename))
+            {
+                return _icon;
+            }
+
+            return null;
+        }
+    }
+}
