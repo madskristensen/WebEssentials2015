5067b57c4d5c905968d0eb46aefd35f09279e0f0
Mads Kristensen <post@madskristensen.net>
2014-11-04 11:20:23 -0800

    JSON/JS: Added logo adornments

---
+++ EditorExtensions/JSON/Adornments/LogoAdornment.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace MadsKristensen.EditorExtensions.JSON
+{
+    class LogoAdornment
+    {
+        private IAdornmentLayer _adornmentLayer;
+        private Image _adornment;
+
+        public LogoAdornment(IWpfTextView view, string imageName)
+        {
+            _adornmentLayer = view.GetAdornmentLayer(LogoProvider.LayerName);
+            CreateImage(imageName);
+
+            view.ViewportHeightChanged += SetAdornmentLocation;
+            view.ViewportWidthChanged += SetAdornmentLocation;
+
+            Update();
+        }
+
+        private void CreateImage(string imageName)
+        {
+            _adornment = new Image();
+            _adornment.Source = BitmapFrame.Create(new Uri("pack://application:,,,/WebEssentials2015;component/JSON/Resources/" + imageName, UriKind.RelativeOrAbsolute));
+            _adornment.ToolTip = "Click to hide";
+            _adornment.Opacity = 0.5D;
+
+            _adornment.MouseEnter += (s, e) => { _adornment.Opacity = 1D; };
+            _adornment.MouseLeave += (s, e) => { _adornment.Opacity = 0.5D; };
+            _adornment.MouseLeftButtonUp += (s, e) => { _adornment.Visibility = Visibility.Hidden; };
+        }
+
+        private void SetAdornmentLocation(object sender, EventArgs e)
+        {
+            IWpfTextView view = (IWpfTextView)sender;
+            Canvas.SetLeft(_adornment, view.ViewportRight - 100);
+            Canvas.SetTop(_adornment, view.ViewportTop + 20);
+        }
+
+        private void Update()
+        {
+            if (_adornmentLayer.IsEmpty)
+                _adornmentLayer.AddAdornment(AdornmentPositioningBehavior.ViewportRelative, null, null, _adornment, null);
+        }
+    }
+}
---
+++ EditorExtensions/JSON/Adornments/LogoProvider.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+
+namespace MadsKristensen.EditorExtensions.JSON
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("json")]
+    [ContentType("javascript")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    class LogoProvider : IWpfTextViewCreationListener
+    {
+        public const string LayerName = "JSON Logo";
+
+        [Import]
+        public ITextDocumentFactoryService TextDocumentFactoryService { get; set; }
+
+        [Export(typeof(AdornmentLayerDefinition))]
+        [Name(LayerName)]
+        [Order(After = PredefinedAdornmentLayers.Caret)]
+        public AdornmentLayerDefinition editorAdornmentLayer = null;
+
+        private Dictionary<string, string> _map = new Dictionary<string, string>()
+        {
+            { "bower.json", "bower.png"},
+            { "package.json", "npm.png"},
+            { "project.json", "vs.png"},
+            { "gruntfile.js", "grunt.png"},
+            { "gulpfile.js", "gulp.png"},
+        };
+
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            ITextDocument document;
+            if (TextDocumentFactoryService.TryGetTextDocument(textView.TextDataModel.DocumentBuffer, out document))
+            {
+                string fileName = Path.GetFileName(document.FilePath);
+
+                if (_map.ContainsKey(fileName))
+                {
+                    LogoAdornment highlighter = new LogoAdornment(textView, _map[fileName]);
+                }
+            }
+        }
+    }
+}
