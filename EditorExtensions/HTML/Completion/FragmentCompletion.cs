7dda84ec97c7a3c8511366f2a5d984755e1e2564
Mads Kristensen <post@madskristensen.net>
2014-11-13 10:23:38 -0800

    HTML: Added fragment meta completion

---
+++ EditorExtensions/HTML/Completion/FragmentCompletion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Html.Editor.Intellisense;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.Web.Editor;
+
+namespace MadsKristensen.EditorExtensions.Html
+{
+    [HtmlCompletionProvider(CompletionType.Values, "meta", "content")]
+    [ContentType(HtmlContentTypeDefinition.HtmlContentType)]
+    public class FragmentCompletion : StaticListCompletion
+    {
+        protected override string KeyProperty { get { return "name"; } }
+        public FragmentCompletion()
+            : base(new Dictionary<string, IEnumerable<string>>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "fragment",  Values("!") }
+            }) { }
+    }
+}
