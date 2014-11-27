7951c7aa673c6616db9af4cba6873b599b334e96
Mads Kristensen <post@madskristensen.net>
2014-11-26 19:02:42 -0800

    v0.1.4

--- EditorExtensions/Properties/AssemblyInfo.cs
+++ EditorExtensions/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Web Essentials 2015")]
@@ -18,16 +18,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 
 [assembly: AssemblyVersion(MadsKristensen.EditorExtensions.WebEssentialsPackage.Version)]
 [assembly: AssemblyFileVersion(MadsKristensen.EditorExtensions.WebEssentialsPackage.Version)]
--- EditorExtensions/WebEssentialsPackage.cs
+++ EditorExtensions/WebEssentialsPackage.cs
@@ -34,7 +34,7 @@ namespace MadsKristensen.EditorExtensions
     [SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling"), PackageRegistration(UseManagedResourcesOnly = true)]
     public sealed class WebEssentialsPackage : Package
     {
-        public const string Version = "0.1.3";
+        public const string Version = "0.1.4";
 
         private static DTE2 _dte;
         private static IVsRegisterPriorityCommandTarget _pct;
