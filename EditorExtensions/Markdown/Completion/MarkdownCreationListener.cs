5ad40bb7c8e63e55eb8e433b546b43343bd6693a
Mads Kristensen <post@madskristensen.net>
2015-04-24 12:50:22 -0700

    Removed unneeded ENTER command

--- EditorExtensions/Markdown/Completion/MarkdownCreationListener.cs
+++ EditorExtensions/Markdown/Completion/MarkdownCreationListener.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.ComponentModel.Composition;
+﻿using System.ComponentModel.Composition;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Editor;
 using Microsoft.VisualStudio.Text.Editor;
-using Microsoft.VisualStudio.Text.Operations;
 using Microsoft.VisualStudio.TextManager.Interop;
 using Microsoft.VisualStudio.Utilities;
 
@@ -16,39 +14,12 @@ namespace MadsKristensen.EditorExtensions.Markdown
 	{
 		[Import]
 		IVsEditorAdaptersFactoryService AdaptersFactory = null;
-		[Import]
-		IEditorOperationsFactoryService OperationsFactory = null;
 
 		public void VsTextViewCreated(IVsTextView textViewAdapter)
 		{
 			IWpfTextView view = AdaptersFactory.GetWpfTextView(textViewAdapter);
 
 			view.Options.SetOptionValue<bool>(DefaultOptions.ConvertTabsToSpacesOptionId, true);
-			
-			view.Properties.GetOrCreateSingletonProperty(() => new CommandFilter(textViewAdapter, view, OperationsFactory.GetEditorOperations(view)));
-		}
-	}
-
-	internal sealed class CommandFilter : CommandTargetBase<VSConstants.VSStd2KCmdID>
-	{
-		private IEditorOperations editorOperations;
-
-		public CommandFilter(IVsTextView adapter, IWpfTextView textView, IEditorOperations editorOperations)
-			: base(adapter, textView, VSConstants.VSStd2KCmdID.RETURN)
-		{
-			this.editorOperations = editorOperations;
-		}
-		
-
-		protected override bool IsEnabled()
-		{
-			return true;
-		}
-
-		protected override bool Execute(VSConstants.VSStd2KCmdID commandId, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
-		{
-			editorOperations.InsertNewLine();
-			return true;
 		}
 	}
 }
