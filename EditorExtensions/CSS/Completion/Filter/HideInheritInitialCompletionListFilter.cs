28dc3d5ab235c40aed5b8453085d33149a514a6d
Mads Kristensen <post@madskristensen.net>
2015-04-24 17:49:57 -0700

    CSS: Support for "all" and "unset"

--- EditorExtensions/CSS/Completion/Filter/HideInheritInitialCompletionListFilter.cs
+++ EditorExtensions/CSS/Completion/Filter/HideInheritInitialCompletionListFilter.cs
@@ -4,6 +4,7 @@ using MadsKristensen.EditorExtensions.Settings;
 using Microsoft.VisualStudio.Utilities;
 using Microsoft.CSS.Editor.Completion;
 using Microsoft.Web.Editor.Completion;
+using Microsoft.CSS.Core.TreeItems;
 
 namespace MadsKristensen.EditorExtensions.Css
 {
@@ -16,9 +17,15 @@ namespace MadsKristensen.EditorExtensions.Css
             if (context.ContextType != CssCompletionContextType.PropertyValue || WESettings.Instance.Css.ShowInitialInherit)
                 return;
 
+            // Only show inherit/initial/unset on the "all" property
+            Declaration dec = context.ContextItem.FindType<Declaration>();
+
+            if (dec != null && dec.PropertyNameText == "all")
+                return;
+
             foreach (CssCompletionEntry entry in completions)
             {
-                if (entry.DisplayText == "initial" || entry.DisplayText == "inherit")
+                if (entry.DisplayText == "initial" || entry.DisplayText == "inherit" || entry.DisplayText == "unset")
                 {
                     entry.FilterType = CompletionEntryFilterTypes.NeverVisible;
                 }
