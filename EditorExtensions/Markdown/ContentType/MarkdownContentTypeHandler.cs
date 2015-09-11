1a131b5387a2fa5d7a262e15d1de2ee521a2a5a7
Mads Kristensen <post@madskristensen.net>
2015-09-10 19:39:11 -0700

    Markdown: ArgumentOutOfRangeException. Fixed #98

--- EditorExtensions/Markdown/ContentType/MarkdownContentTypeHandler.cs
+++ EditorExtensions/Markdown/ContentType/MarkdownContentTypeHandler.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Linq;
-using System.Reflection;
+﻿using System.ComponentModel.Composition;
 using Microsoft.Html.Core.Artifacts;
-using Microsoft.Html.Editor.ContainedLanguage.Common;
-using Microsoft.Html.Editor.ContainedLanguage.Generators;
-using Microsoft.Html.Editor.ContainedLanguage.Handlers;
 using Microsoft.Html.Editor.ContentType.Def;
 using Microsoft.Html.Editor.ContentType.Handlers;
-using Microsoft.Html.Editor.Tree;
 using Microsoft.VisualStudio.Utilities;
 using Microsoft.Web.Editor.Formatting;
 
@@ -19,90 +11,18 @@ namespace MadsKristensen.EditorExtensions.Markdown
     [ContentType(MarkdownContentTypeDefinition.MarkdownContentType)]
     public class MarkdownContentTypeHandlerProvider : IContentTypeHandlerProvider
     {
-        [Import]
-        public IContentTypeRegistryService ContentTypeRegistry { get; set; }
-
-        public IContentTypeHandler GetContentTypeHandler() { return new MarkdownContentTypeHandler(ContentTypeRegistry); }
+        public IContentTypeHandler GetContentTypeHandler()
+        {
+            return new MarkdownContentTypeHandler();
+        }
     }
 
     public class MarkdownContentTypeHandler : HtmlContentTypeHandler
     {
-        static readonly Func<HtmlContentTypeHandler, List<LanguageBlockHandler>> GetLanguageBlockHandlerList =
-            (Func<HtmlContentTypeHandler, List<LanguageBlockHandler>>)
-            Delegate.CreateDelegate(
-                typeof(Func<HtmlContentTypeHandler, List<LanguageBlockHandler>>),
-                typeof(HtmlContentTypeHandler).GetProperty("LanguageBlockHandlers", BindingFlags.NonPublic | BindingFlags.Instance).GetMethod
-            );
-
-        readonly IContentTypeRegistryService contentTypeRegistry;
-        public MarkdownContentTypeHandler(IContentTypeRegistryService contentTypeRegistry)
-        {
-            this.contentTypeRegistry = contentTypeRegistry;
-        }
-
         public override ArtifactCollection CreateArtifactCollection()
         {
             return new MarkdownCodeArtifactCollection(new MarkdownCodeArtifactProcessor());
         }
-
-        protected override void CreateBlockHandlers()
-        {
-            base.CreateBlockHandlers();
-            GetLanguageBlockHandlerList(this).Add(new CodeBlockBlockHandler(EditorTree, contentTypeRegistry));
-        }
-
-        public override void UpdateContainedLanguageBuffers()
-        {
-            // TODO: Call RemoveSpans() on each created LanguageProjectionBuffer iff IsRegenerationNeeded()
-            base.UpdateContainedLanguageBuffers();
-        }
-    }
-
-    class CodeBlockBlockHandler : ArtifactBasedBlockHandler
-    {
-        readonly IContentTypeRegistryService contentTypeRegistry;
-        public CodeBlockBlockHandler(HtmlEditorTree tree, IContentTypeRegistryService contentTypeRegistry)
-            : base(tree, contentTypeRegistry.GetContentType("code"))
-        {
-            this.contentTypeRegistry = contentTypeRegistry;
-        }
-        protected override BufferGenerator CreateBufferGenerator()
-        {
-            return new ArtifactBasedBufferGenerator(EditorTree, LanguageBlocks, contentTypeRegistry.GetContentType("text"));
-        }
-        public override IContentType GetContentTypeOfLocation(int position)
-        {
-            LanguageBlock block = this.GetLanguageBlockOfLocation(position);
-            if (block == null) return null;
-
-            var alb = block as ArtifactLanguageBlock;
-
-            if (alb == null)
-                return contentTypeRegistry.GetContentType("text");
-
-            return alb.ContentType ?? contentTypeRegistry.GetContentType("code");
-        }
-
-        protected override void BuildLanguageBlockCollection()
-        {
-            LanguageBlocks.Clear();
-            foreach (var artifact in EditorTree.RootNode.Tree.ArtifactCollection.OfType<CodeLineArtifact>())
-            {
-                var contentType = contentTypeRegistry.GetContentType("code");
-                LanguageBlocks.AddBlock(new ArtifactLanguageBlock(artifact, contentType));
-            }
-            LanguageBlocks.SortByPosition();
-        }
-    }
-
-    class ArtifactLanguageBlock : LanguageBlock
-    {
-        public ArtifactLanguageBlock(CodeLineArtifact a, IContentType contentType)
-            : base(a)
-        {
-            ContentType = contentType;
-        }
-        public IContentType ContentType { get; private set; }
     }
 
     // The HTML formatter doesn't work properly with Artifacts
